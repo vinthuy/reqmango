551a7abe9d1c136d486851ca2747764fbbc9b8b8
vinthuy <vinthuy@qq.com>
2026-07-19 16:04:25 +0800

    chore: add approval testing scripts and E2E tests - Added genhash.go and reset_password.go for password management - Added workflow-approval E2E test files - Updated playwright config

---
+++ backend/genhash.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	"golang.org/x/crypto/bcrypt"
+)
+
+func main() {
+	h, _ := bcrypt.GenerateFromPassword([]byte("demo1234"), bcrypt.DefaultCost)
+	fmt.Println(string(h))
+}
